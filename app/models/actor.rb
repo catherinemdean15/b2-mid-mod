a77ea287d8b01c755bfde6ccc591e5b074a97960
Catherine Dean <catherinemdean15@gmail.com>
2021-01-04 11:06:36 -0700

    create actors_worked_with action

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -2,4 +2,11 @@ class Actor <ApplicationRecord
   has_many :movie_actors
   has_many :movies, through: :movie_actors
 
+
+  def actors_worked_with
+    movies.map do |movie|
+      movie.actors
+    end.flatten.uniq
+  end
+
 end
