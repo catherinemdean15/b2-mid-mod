b2c3cdf119482dc29fb35fb7524b8f8cbd8eed8c
Catherine Dean <catherinemdean15@gmail.com>
2021-01-04 10:44:24 -0700

    create update action in movies controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,5 +4,12 @@ class MoviesController < ApplicationController
     @movie = Movie.find(params[:id])
   end
 
+  def update
+    actor = Actor.where("#{"name"} like ?", "%#{params[:actor]}%")
+    movie = Movie.find(params[:id])
+    MovieActor.create!(actor_id: actor[0].id, movie_id: movie.id)
+    redirect_to "/movies/#{movie.id}"
+  end
+
 
 end
